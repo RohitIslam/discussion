2567211f8ba940183b512b29a11ab9dcd66a942a
Rohit Islam <air.rohit999@gmail.com>
2019-08-20 14:38:20 +0000

    API created for adding and deleting post by ID

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -19,7 +19,7 @@ router.get("/test", (req, res) =>
   res.json({ msg: "test route works for posts" })
 );
 
-// @route GET api/post/all
+// @route GET api/post/
 // @description get all posts
 // @access Public
 
@@ -30,12 +30,32 @@ router.get("/", (req, res) => {
     .populate("user", ["name", "avatar"])
     .then(posts => {
       if (!posts) {
-        errors.noProfile = "There are no posts";
-        res.status(404).json(errors);
+        errors.noPost = "There are no posts";
+        return res.status(404).json(errors);
       }
       res.json(posts);
     })
     .catch(err => res.status(400).json({ error: "There are no posts" }));
+});
+
+// @route GET api/post/:id
+// @description get post by id
+// @access Public
+
+router.get("/:id", (req, res) => {
+  const errors = {};
+  Post.findById(req.params.id)
+    .populate("user", ["name", "avatar"])
+    .then(post => {
+      if (!post) {
+        errors.noPost = "No post found with that ID";
+        return res.status(404).json(errors);
+      }
+      res.json(post);
+    })
+    .catch(err =>
+      res.status(400).json({ error: "No post found with that ID" })
+    );
 });
 
 // GET API END
@@ -70,6 +90,37 @@ router.post(
 // POST API END
 
 // DELETE API START
+
+// @route DELETE api/post/:id
+// @description DELETE post by id
+// @access Private
+
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        Post.findById(req.params.id)
+          .then(post => {
+            // check for the post owner
+            if (post.user.toString() !== req.user.id) {
+              errors.notAuthorized = "User not authorized";
+              return res.status(401).json(errors);
+            }
+            //Delete
+            post
+              .remove()
+              .then(() => res.json({ success: "Post successfully deleted" }));
+          })
+          .catch(err => res.status(400).json({ errors: "Post not found" }));
+      })
+      .catch(err => res.status(400).json({ errors: "Profile not found" }));
+  }
+);
+
 // DELETE API END
 
 module.exports = router;
