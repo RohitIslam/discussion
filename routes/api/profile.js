08fb41f27adf2f67d4d24bd28456218c88311e03
Rohit Islam <air.rohit999@gmail.com>
2019-08-20 06:18:56 +0000

    get profile by handle and get profile by user id API created

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -39,6 +39,42 @@ router.get(
   }
 );
 
+// @route GET api/profile/handle/:handle
+// @description get profile by handle
+// @access Public
+
+router.get("/handle/:handle", (req, res) => {
+  const errors = {};
+  Profile.findOne({ handle: req.params.handle })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noProfile = "There is no profile for this user";
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(400).json(err));
+});
+
+// @route GET api/profile/user/:user_id
+// @description get profile by user id
+// @access Public
+
+router.get("/user/:user_id", (req, res) => {
+  const errors = {};
+  Profile.findOne({ user: req.params.user_id })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noProfile = "There is no profile for this user";
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(400).json(err));
+});
+
 // @route POST api/profile
 // @description Create or Edit user profile
 // @access Private
