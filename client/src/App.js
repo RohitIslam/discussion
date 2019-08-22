b0ded5467adb9af4ce42daef7d01c0b76f56473e
Rohit Islam <air.rohit999@gmail.com>
2019-08-22 14:09:20 +0000

    CreateProfile component added adn route created

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Login from "./Components/Auth/Login";
 import Register from "./Components/Auth/Register";
 import Dashboard from "./Components/Dashboard/Dashboard";
 import PrivateRoute from "./store/utils/PrivateRoute/PrivateRoute";
+import CreateProfile from "./Components/CreateProfile/CreateProfile";
 
 class App extends Component {
   state = {};
@@ -17,6 +18,11 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <Switch>
+          <PrivateRoute
+            path="/create-profile"
+            exact
+            component={CreateProfile}
+          />
           <PrivateRoute path="/dashboard" exact component={Dashboard} />
           <Route path="/" exact component={Landing} />
         </Switch>
---
+++ client/src/Components/CreateProfile/CreateProfile.js
@@ -0,0 +1,242 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+
+import * as actions from "../../store/actions/indexActions";
+
+class CreateProfile extends Component {
+  state = {
+    displaySocialInputs: false,
+    handle: "",
+    company: "",
+    website: "",
+    location: "",
+    status: "",
+    skills: "",
+    gitHubUserName: "",
+    bio: "",
+    twitter: "",
+    facebook: "",
+    youtube: "",
+    linkedin: "",
+    instagram: "",
+    errors: {}
+  };
+  render() {
+    return (
+      <div className="create-profile">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <a href="dashboard.html" className="btn btn-light">
+                Go Back
+              </a>
+              <h1 className="display-4 text-center">Create Your Profile</h1>
+              <small className="d-block pb-3">* = required field</small>
+              <form action="add-experience.html">
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="* Profile handle"
+                    name="handle"
+                    required
+                  />
+                  <small className="form-text text-muted">
+                    A unique handle for your profile URL. Your full name,
+                    company name, nickname, etc (This CAN'T be changed later)
+                  </small>
+                </div>
+                <div className="form-group">
+                  <select
+                    className="form-control form-control-lg"
+                    name="status"
+                  >
+                    <option value="0">* Select Professional Status</option>
+                    <option value="Developer">Developer</option>
+                    <option value="Junior Developer">Junior Developer</option>
+                    <option value="Senior Developer">Senior Developer</option>
+                    <option value="Manager">Manager</option>
+                    <option value="Student or Learning">
+                      Student or Learning
+                    </option>
+                    <option value="Instructor">Instructor or Teacher</option>
+                    <option value="Intern">Intern</option>
+                    <option value="Other">Other</option>
+                  </select>
+                  <small className="form-text text-muted">
+                    Give us an idea of where you are at in your career
+                  </small>
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Company"
+                    name="company"
+                  />
+                  <small className="form-text text-muted">
+                    Could be your own company or one you work for
+                  </small>
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Website"
+                    name="website"
+                  />
+                  <small className="form-text text-muted">
+                    Could be your own or a company website
+                  </small>
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Location"
+                    name="location"
+                  />
+                  <small className="form-text text-muted">
+                    City & state suggested (eg. Boston, MA)
+                  </small>
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Skills"
+                    name="skills"
+                  />
+                  <small className="form-text text-muted">
+                    Please use comma separated values (eg.
+                    HTML,CSS,JavaScript,PHP)
+                  </small>
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Github Username"
+                    name="githubusername"
+                  />
+                  <small className="form-text text-muted">
+                    If you want your latest repos and a Github link, include
+                    your username
+                  </small>
+                </div>
+                <div className="form-group">
+                  <textarea
+                    className="form-control form-control-lg"
+                    placeholder="A short bio of yourself"
+                    name="bio"
+                  />
+                  <small className="form-text text-muted">
+                    Tell us a little about yourself
+                  </small>
+                </div>
+
+                <div className="mb-3">
+                  <button type="button" className="btn btn-light">
+                    Add Social Network Links
+                  </button>
+                  <span className="text-muted">Optional</span>
+                </div>
+
+                <div className="input-group mb-3">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text">
+                      <i className="fab fa-twitter" />
+                    </span>
+                  </div>
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Twitter Profile URL"
+                    name="twitter"
+                  />
+                </div>
+
+                <div className="input-group mb-3">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text">
+                      <i className="fab fa-facebook" />
+                    </span>
+                  </div>
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Facebook Page URL"
+                    name="facebook"
+                  />
+                </div>
+
+                <div className="input-group mb-3">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text">
+                      <i className="fab fa-linkedin" />
+                    </span>
+                  </div>
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Linkedin Profile URL"
+                    name="linkedin"
+                  />
+                </div>
+
+                <div className="input-group mb-3">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text">
+                      <i className="fab fa-youtube" />
+                    </span>
+                  </div>
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="YouTube Channel URL"
+                    name="youtube"
+                  />
+                </div>
+
+                <div className="input-group mb-3">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text">
+                      <i className="fab fa-instagram" />
+                    </span>
+                  </div>
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Instagram Page URL"
+                    name="instagram"
+                  />
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    auth: state.auth,
+    profile: state.profile,
+    errors: state.errors
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onLoginSubmit: userInfo => dispatch(actions.login(userInfo))
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CreateProfile);
