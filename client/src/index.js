8a297114e7e9b716dfed179fbbb46f73264ffb97
Rohit Islam <air.rohit999@gmail.com>
2019-08-26 05:05:21 +0000

    Store and rootReducer setted up for redux

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-// import { Provider } from "react-redux";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 ReactDOM.render(app, document.getElementById("root"));
---
+++ client/src/store/reducers/rootReducer.js
@@ -0,0 +1,14 @@
+import { combineReducers } from "redux";
+
+// import authReducer from "./authReducer";
+// import errorReducer from "./errorReducer";
+// import profileReducer from "./profileReducer";
+
+const rootReducer = combineReducers({});
+// const rootReducer = combineReducers({
+//     auth: authReducer,
+//     profile: profileReducer,
+//     errors: errorReducer
+// });
+
+export default rootReducer;
