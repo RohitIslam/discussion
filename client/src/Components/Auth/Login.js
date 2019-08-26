7d2d7cfc7faac333658ef7472988b24db2593016
Rohit Islam <air.rohit999@gmail.com>
2019-08-26 04:50:57 +0000

    Login form setup

--- client/src/Components/Auth/Login.js
+++ client/src/Components/Auth/Login.js
@@ -1,10 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
+
+  const { email, password } = formData;
+
+  const changeHandler = event => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const formSubmitHandler = event => {
+    event.preventDefault();
+    console.log(formData);
+    // const newUser = {
+    //   name: name,
+    //   email: email,
+    //   password: password,
+    //   password2: password2
+    // };
+    // this.props.onRegistrationSubmit(newUser, this.props.history);
+  };
+
   return (
-    <div>
-      <h1>log</h1>
-    </div>
+    <React.Fragment>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Sign Into Your Account
+      </p>
+      <form className="form" onSubmit={e => formSubmitHandler(e)}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            value={email}
+            onChange={changeHandler}
+            name="email"
+            required
+          />
+          <small className="form-text">
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={changeHandler}
+            minLength="6"
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Register</Link>
+      </p>
+    </React.Fragment>
   );
 };
 
