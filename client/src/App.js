3a2c72980a23c24c6b93ed69b7c1a4b7255472d5
Rohit Islam <air.rohit999@gmail.com>
2019-08-26 04:03:32 +0000

    React router setup in Navbar and Landing components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import React, { Fragment } from "react";
+import { Route, Switch } from "react-router-dom";
+
 import "./App.css";
 import Navbar from "./Components/Layouts/Navbar";
 import Landing from "./Components/Layouts/Landing";
+import Login from "./Components/Auth/Login";
+import Register from "./Components/Auth/Register";
 
 function App() {
   return (
     <Fragment>
       <Navbar />
-      <Landing />
+      <Route path="/" exact component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route path="/register" exact component={Register} />
+          <Route path="/login" exact component={Login} />
+        </Switch>
+      </section>
     </Fragment>
   );
 }
--- client/src/Components/Layouts/Navbar.js
+++ client/src/Components/Layouts/Navbar.js
@@ -1,22 +1,24 @@
 import React from "react";
+// import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="navbar bg-dark">
       <h1>
-        <a href="index.html">
+        <Link to="/">
           <i className="fas fa-sitemap"></i> Discussion
-        </a>
+        </Link>
       </h1>
       <ul>
         <li>
-          <a href="profiles.html">Developers</a>
+          <Link href="profiles.html">Developers</Link>
         </li>
         <li>
-          <a href="register.html">Register</a>
+          <Link to="/register">Register</Link>
         </li>
         <li>
-          <a href="login.html">Login</a>
+          <Link to="/login">Login</Link>
         </li>
       </ul>
     </nav>
